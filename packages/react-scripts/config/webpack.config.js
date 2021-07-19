969655c0c9d1ba1c9bffc276fc4985812c2da6fc
EugeneKey <evgeny.konyaev@gmail.com>
2021-07-20 05:29:57 +0800

    update webpack.config

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -335,6 +335,7 @@ module.exports = function(webpackEnv) {
         }),
         ...(modules.webpackAliases || {}),
       },
+      aliasFields: ["module"],
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
         // guards against forgotten dependencies and such.
